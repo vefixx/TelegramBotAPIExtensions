09385705942f40912caaaa8fce349d7f2e162d9a
vefixx <vef1xx@yandex.ru>
2025-10-19 20:19:28 +0500

    Тестовый обработки состояний

---
+++ Tests/Program.cs
@@ -0,0 +1,59 @@
+﻿using Telegram.BotAPI;
+using Telegram.BotAPI.AvailableMethods;
+using Telegram.BotAPI.GettingUpdates;
+using TelegramBotAPIExtensions.Core.FSM;
+
+namespace Tests;
+
+class Program
+{
+    private TelegramBotClient _client;
+    private long _testUserId = 0;
+    
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        DotNetEnv.Env.Load();
+        await new Program().RunClientAsync();
+    }
+
+    public async Task RunClientAsync()
+    {
+        _client = new TelegramBotClient(DotNetEnv.Env.GetString("TOKEN"));
+        Console.WriteLine($"Start {_client.GetMe().Username}");
+        
+        // Обязательное создание экземпляра FSM. В конструкторе он будет автоматически установлен в Memory
+        FsmService fsmService = new FsmService(_client);
+        
+        // Устанавливаем тестовое состояние
+        fsmService.SetState(_testUserId, "wait_mes1");
+        
+        // Слушаем обновления
+        var updates = await _client.GetUpdatesAsync();
+        if (updates.Any())
+            updates = await _client.GetUpdatesAsync(updates.Last().UpdateId + 1);
+        
+        while (true)
+        {
+            if (updates.Any())
+            {
+                foreach (var update in updates)
+                {
+                    // Проверяем, что у пользователя установлено состояние
+                    UserState? state = fsmService.GetState(_testUserId);
+                    if (state != null)
+                    {
+                        // В нашем случае вызовется метод TestStateHandler.TestHandleAsync
+                        bool stateIsExecuted = await fsmService.TryExecuteCallbackAsync(state.State, update);
+                        Console.WriteLine($"Executed: {stateIsExecuted}");
+                    }
+                }
+                updates = await _client.GetUpdatesAsync(updates.Last().UpdateId + 1);
+            }
+            else
+            {
+                updates = await _client.GetUpdatesAsync();
+            }
+        }
+    }
+}
---
+++ Tests/TestStateHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using TelegramBotAPIExtensions.Core;
+using TelegramBotAPIExtensions.Core.Attributes;
+using TelegramBotAPIExtensions.Core.FSM;
+
+namespace Tests;
+
+public class TestStateHandler : StateHandler
+{
+    [StateCallback("wait_mes1")]
+    public async Task TestHandleAsync(InteractionContext ctx)
+    {
+        Console.WriteLine($"User {ctx.From.Id} send {ctx.Message.Text}");
+        _fsmService.ClearState(ctx.From.Id);    // Очистка состояния
+
+        UserState? state = _fsmService.GetState(ctx.From.Id);   // Для проверки получим текущее состояние
+        Console.WriteLine($"State is null after clear = {state == null}");
+    }
+}
